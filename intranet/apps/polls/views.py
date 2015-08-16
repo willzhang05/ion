0109f1f584ae28097818071845ef11bbf59b4e35
ctrlshiftq <2017sbradbur@tjhsst.edu>
2015-08-15 22:29:56 -0400

    hide polls in production

--- intranet/apps/polls/views.py
+++ intranet/apps/polls/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 import logging
+from intranet import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
@@ -10,4 +11,8 @@ logger = logging.getLogger(__name__)
 
 @login_required
 def polls_view(request):
+
+    if settings.PRODUCTION and not request.user.has_admin_permission('polls'):
+        return render(request, "polls/not_ready.html")
+
     return render(request, "polls/polls.html")
